c4ef3f91e384189046b2b20e113af7cb37161713
Tom Clark <tom@octue.com>
2023-09-13 10:54:02 +0100

    FIX: Update test description

--- test/test_design_basis.py
+++ test/test_design_basis.py
@@ -10,13 +10,13 @@ def subschema(schema):
     return get_subschema(schema, "design_basis")
 
 
-def test_generic_turbine_metadata(subschema, generic_design_basis):
-    """Validation should pass on the generic turbine metadata"""
+def test_generic_design_basis(subschema, generic_design_basis):
+    """Validation should pass on the generic turbine design basis"""
     validate(instance=generic_design_basis, schema=subschema)
 
 
 def test_missing_design_basis(subschema):
-    """Validation should fail if there is no turbine metadata section"""
+    """Validation should fail if there is no design basis section"""
     with pytest.raises(ValidationError) as e:
         validate(instance={}, schema=subschema)
     assert "'design_basis' is a required property" in str(e)
